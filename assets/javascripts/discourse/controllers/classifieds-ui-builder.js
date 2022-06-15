5f84f7e561a4f0d444d99ac523313f0375a2f260
Keegan George <kgeorge13@gmail.com>
2022-06-15 14:45:03 -0700

    🚧 (WIP): Build listing markup

--- assets/javascripts/discourse/controllers/classifieds-ui-builder.js
+++ assets/javascripts/discourse/controllers/classifieds-ui-builder.js
@@ -3,8 +3,11 @@ import ModalFunctionality from "discourse/mixins/modal-functionality";
 import { action } from "@ember/object";
 import discourseComputed from "discourse-common/utils/decorators";
 import I18n from "I18n";
+import { A } from "@ember/array";
 
 export default Controller.extend(ModalFunctionality, {
+  listingImages: A(),
+
   @discourseComputed
   conditionTypes() {
     const options = [
@@ -31,16 +34,66 @@ export default Controller.extend(ModalFunctionality, {
     return options;
   },
 
+  @discourseComputed(
+    "title",
+    "description",
+    "price",
+    "location",
+    "condition",
+    "listingImages"
+  )
+  listingOutput(title, description, price, loc, condition, images) {
+    let header = "[classifiedListing";
+    let output = "";
+
+    if (title) {
+      header += ` title="${title}"`;
+    }
+
+    if (price) {
+      header += ` price="${price}"`;
+    }
+
+    if (condition) {
+      header += ` condition="${condition}"`;
+    }
+
+    if (loc) {
+      header += ` location="${loc}"`;
+    }
+
+    header += "]";
+    output += `${header}\n`;
+
+    // Output
+    if (description) {
+      output += `${description.trim()}\n`;
+    }
+
+    if (images) {
+      images.forEach((image, index) => {
+        const buildImageFile = `![${image.file_name}|${image.width}x${image.height}](${image.short_url})\n`;
+        if (index === 0) {
+          output += `[coverImage]\n${buildImageFile}[/coverImage]\n`;
+        } else {
+          output += buildImageFile;
+        }
+      });
+    }
+
+    output += "[/classifiedListing]\n";
+    return output;
+  },
+
   @action
   createListing() {
-    // this.toolbarEvent.addText(this.listingOutput);
-    this.toolbarEvent.addText("My new listing! YAY");
+    this.toolbarEvent.addText(this.listingOutput);
     this.send("closeModal");
   },
 
   @action
   uploadDone(upload) {
-    //eslint-disable-next-line no-console
-    console.log("upload file", upload);
+    const listingImages = this.listingImages;
+    listingImages.pushObject(upload);
   },
 });
